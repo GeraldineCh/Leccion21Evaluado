3ed8fad01788a29b6fde99d71cbd997d550c903e
Geraldine Chauca <gchaucae@gmail.com>
2017-04-17 17:14:18 -0500

    Arreglo de posicionamiento

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,14 @@
+//Para que cierren las imágenes
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+    modal.style.display = "none";}
+
+//Para que desaparezca la segunda columna con botón Extinción
 document.getElementById("ex").addEventListener("click", function(){
     document.getElementById("extincion").style.visibility = "hidden";
 });
 
+//Para que aparezca la segunda columna con botón Origen
 document.getElementById("or").addEventListener("click", function(){
     document.getElementById("extincion").style.visibility = "visible";
 });
