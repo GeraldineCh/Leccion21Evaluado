dfd0142b3f7fdbbd82d0e61ebc9c4de279b0435e
Geraldine Chauca <gchaucae@gmail.com>
2017-04-17 16:54:32 -0500

    Colocación de párrafos y funcionalidad de botones

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+document.getElementById("ex").addEventListener("click", function(){
+    document.getElementById("extincion").style.visibility = "hidden";
+});
+
+document.getElementById("or").addEventListener("click", function(){
+    document.getElementById("extincion").style.visibility = "visible";
+});
