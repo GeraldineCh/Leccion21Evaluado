8b380c3e32eac41c16d7c03a8ae9d1105ff4e554
Geraldine Chauca <gchaucae@gmail.com>
2017-04-17 18:13:35 -0500

    Refactorizando funcionalidad de botones

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,22 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";}
 
-//Para que desaparezca la segunda columna con botón Extinción
+//Para que desaparezca la primera columna con botón Extinción
 document.getElementById("ex").addEventListener("click", function(){
-    document.getElementById("extincion").style.visibility = "hidden";
+    if (document.getElementById("extincion").style.visibility = "hidden")
+    	{document.getElementById("extincion").style.visibility = "visible";}
+    else {
+    	document.getElementById("extincion").style.visibility = "hidden"
+    }
+
 });
 
 //Para que aparezca la segunda columna con botón Origen
 document.getElementById("or").addEventListener("click", function(){
-    document.getElementById("extincion").style.visibility = "visible";
+    if (document.getElementById("origen").style.visibility = "hidden")
+    	{document.getElementById("origen").style.visibility = "visible";}
+    else {
+    	document.getElementById("origen").style.visibility = "hidden"
+    }
+
 });
