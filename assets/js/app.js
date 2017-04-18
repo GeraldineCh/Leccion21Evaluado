f91fae8609d59ebf6232478aef24a07f603c40c4
Geraldine Chauca <gchaucae@gmail.com>
2017-04-18 11:16:36 -0500

    Funcionalidades de botones respecto a párrafos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,22 @@
-//Para que cierren las imágenes
+/*Para que cierren las imágenes
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
-    modal.style.display = "none";}
+    modal.style.display = "none";}*/
 
-//Para que desaparezca la primera columna con botón Extinción
-document.getElementById("ex").addEventListener("click", function(){
-    if (document.getElementById("extincion").style.visibility = "hidden")
-    	{document.getElementById("extincion").style.visibility = "visible";}
+//Para primera columna con botón Origen
+document.getElementById("or").addEventListener("click", function(){
+    if (document.getElementById("origen").style.visibility == "hidden") {
+    	    document.getElementById("origen").style.visibility = "visible";}
     else {
-    	document.getElementById("extincion").style.visibility = "hidden"
+    	    document.getElementById("origen").style.visibility = "hidden";
     }
-
 });
 
-//Para que aparezca la segunda columna con botón Origen
-document.getElementById("or").addEventListener("click", function(){
-    if (document.getElementById("origen").style.visibility = "hidden")
-    	{document.getElementById("origen").style.visibility = "visible";}
+//Para segunda columna con botón Extinción
+document.getElementById("ex").addEventListener("click", function(){
+    if (document.getElementById("extincion").style.visibility == "hidden") {
+    	    document.getElementById("extincion").style.visibility = "visible";}
     else {
-    	document.getElementById("origen").style.visibility = "hidden"
+    	    document.getElementById("extincion").style.visibility = "hidden";
     }
-
 });
